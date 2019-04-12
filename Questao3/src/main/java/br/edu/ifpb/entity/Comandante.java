31f57f54ea2950d6d7fc02056fdc561b6e8ef864
MurilloTavares <murillo_eum@hotmail.com>
2019-04-12 02:17:26 -0300

    Backup questao 3

---
+++ Questao3/src/main/java/br/edu/ifpb/entity/Comandante.java
@@ -0,0 +1,12 @@
+package br.edu.ifpb.entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+
+//@Entity
+public class Comandante implements Serializable {
+
+//    @Id
+//    @GeneratedValue
+    
+}
